7176e8725b5c7eda81f4aea3e62c583b09feff88
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-09-05 11:09:24 -0700

    initial commit.

--- plugins/kin-athletics-functionality/kin-athletics-functionality.php
+++ plugins/kin-athletics-functionality/kin-athletics-functionality.php
@@ -1,16 +1,16 @@
 <?php
  /**
  *
- * @package   [SITE NAME] Functionality
- * @author    Your Name <your_email@email.com>
+ * @package   Kin Athletics Functionality
+ * @author    Aleezay Hashmi, Ken CHow, Mark Flores, Matthew Cuthbert
  * @license   GPL-2.0+
- * @copyright 2015 Your Name or Company
+ * @copyright 2017 
  *
  * @wordpress-plugin
- * Plugin Name: [SITE NAME] Functionality
+ * Plugin Name: Kin Athletics Functionality
  * Description: This very important plugin contains all of the core functionality for this website so that it remains theme-independent.
  * Version:     1.0.0
- * Author:      Your Name
+ * Author:      Aleezay Hashmi, Ken CHow, Mark Flores, Matthew Cuthbert
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
