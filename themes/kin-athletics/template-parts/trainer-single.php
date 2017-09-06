f3d45ab8aa180095a6c155592f6450bb82c3bce7
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-09-05 21:35:33 -0700

    added trainer-archive and trainer-single.php

---
+++ themes/kin-athletics/template-parts/trainer-single.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template part for displaying a single trainer.
+ *
+ * @package Kin_Athletics_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+        <?php echo CFS()->get( 'trainer_description' ); ?>
+        <?php 
+            foreach (CFS()->get( 'trainer_availability' ) as $date)
+            echo $date;
+            unset($date); // break the reference with the last element 
+            wp_reset_postdata();
+        ?>
+
+        <!-- Personalities  -->
+        <?php echo CFS()->get( 'trainer_personalities' ); ?>
+        <?php
+            $args = array( 'post_type' => 'trainer', 'order' => 'DESC', 'posts_per_page' => 6, 'orderby' => 'date' );
+            $trainer = get_posts( $args ); // returns an array of posts
+            
+            foreach ( $trainer as $post ) : setup_postdata( $post ); ?>
+                <div class="trainer-thumbnail-wrapper">
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <?php the_post_thumbnail( 'large' ); ?>
+                    <?php endif; ?><!-- has_post_thumbnail -->         
+                </div><!-- .journal-thumbnail-wrapper -->
+
+                <div class="entry-wrapper">
+                    <h3><a class="trainer-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>
+                </div><!-- .entry-wrapper -->
+        <?php endforeach; wp_reset_postdata(); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php kin_athletics_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
