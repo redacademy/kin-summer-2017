c1b7463d59143f2d3e1bb374a45a903f17637a46
aleezay <aleezayh@hotmail.com>
2017-09-06 13:27:04 -0700

    html mark up for landing page, starting styling

---
+++ themes/kin-athletics/front-page.php
@@ -0,0 +1,35 @@
+<?php
+/** 
+* Template Name: Landing Page
+*@package Kin_Theme
+**/
+get_header(); ?>
+
+
+<div class="landing-page-container">
+ 
+  <div class="landing1-container">
+    <div class="landing-td-image"></div>
+    <h2>Kin Athletics Presents</h2>
+    <h1>Project TD</h1>
+  </div>
+
+  <div class="scroll-section">
+    <p>Jump In</p>
+  </div>
+
+  <div class="landing2-container">
+    <div class="who-we-are-image"></div>
+    <h2>Who We Are</h2>
+    <p>Ipsum</p>
+  </div>
+
+<div class="landing3-container">
+    <div class="what-we-do-image"></div>
+    <h2>What We Do</h2>
+    <p>Ipsum</p>
+  </div>
+
+</div><!--end of landing-page-container-->
+
+<?php get_footer(); ?>
