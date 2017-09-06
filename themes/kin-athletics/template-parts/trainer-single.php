19869cef850d01232b0a209eaf3ffef7ead4368b
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-09-05 21:40:38 -0700

    added comments to trainer-single.

--- themes/kin-athletics/template-parts/trainer-single.php
+++ themes/kin-athletics/template-parts/trainer-single.php
@@ -17,16 +17,18 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+        <!-- Description -->
         <?php echo CFS()->get( 'trainer_description' ); ?>
+        <!-- Personalities  -->
+        <?php echo CFS()->get( 'trainer_personalities' ); ?>
+        <!-- Availability  -->
         <?php 
             foreach (CFS()->get( 'trainer_availability' ) as $date)
             echo $date;
             unset($date); // break the reference with the last element 
             wp_reset_postdata();
         ?>
-
-        <!-- Personalities  -->
-        <?php echo CFS()->get( 'trainer_personalities' ); ?>
+        <!-- Other Trainers -->
         <?php
             $args = array( 'post_type' => 'trainer', 'order' => 'DESC', 'posts_per_page' => 6, 'orderby' => 'date' );
             $trainer = get_posts( $args ); // returns an array of posts
@@ -36,15 +38,11 @@
                     <?php if ( has_post_thumbnail() ) : ?>
                         <?php the_post_thumbnail( 'large' ); ?>
                     <?php endif; ?><!-- has_post_thumbnail -->         
-                </div><!-- .journal-thumbnail-wrapper -->
+                </div><!-- .trainer-thumbnail-wrapper -->
 
                 <div class="entry-wrapper">
                     <h3><a class="trainer-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>
                 </div><!-- .entry-wrapper -->
         <?php endforeach; wp_reset_postdata(); ?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php kin_athletics_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
