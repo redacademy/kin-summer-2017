718e71a60457a9b1db323c639f90673637052363
Mark <mark@Marks-MacBook-Pro.local>
2017-09-08 16:35:18 -0700

    fully styled events page

--- themes/kin-athletics/events.php
+++ themes/kin-athletics/events.php
@@ -18,22 +18,57 @@ get_header(); ?>
           <h1> make history with us </h1>
 
           <div class='circles'>
-            <svg height="180" width="180">
-              <circle cx="80" cy="80" r="80" fill="red" />
+            <svg height="130" width="130">
+              <circle cx="65" cy="65" r="65" fill="red" />
             </svg>
+            <p> past events </p>
 
-            <svg height="180" width="180">
-              <circle cx="80" cy="80" r="80" fill="red" />
+            <svg height="130" width="130">
+              <circle cx="65" cy="65" r="65" fill="red" />
             </svg>
+            <p> on noe - project td </p>            
 
-            <svg height="180" width="180">
-              <circle cx="80" cy="80" r="80" fill="red" />
+            <svg height="130" width="130">
+              <circle cx="65" cy="65" r="65" fill="red" />
             </svg>
-
-
-
+            <p> buy tickets </p>            
+            
           </div>
         </section>
+
+        <div class='relative-carousel-container'>       
+          <div class="main-carousel">
+            <div class='carousel-cell events-cell-image1'>
+            </div>
+            <div class='carousel-cell events-cell-image2'>
+            </div>
+            <div class='carousel-cell events-cell-image3'>
+            </div>       
+          </div>
+
+          <div class='events-carousel-info'>
+            <?php echo CFS()->get('current_event'); ?>            
+            <?php echo CFS()->get('current_event_info'); ?>
+            <h3 class='event-date'>happens: <?php echo CFS()->get('current_event_date'); ?></h3>
+            <div class='red-button'><a href='#'>buy tickets</a></div>
+          </div>
+        </div> <!-- end of relative carousel container -->
+
+
+        <div class='events-black-container'>
+          <p>but wait,</p>
+          <div class='red-button'><a href='#'>who are we?</a></div>
+        </div>
+
+        <div class='events-white-container'>
+          <p>see our history</p>
+          <div class='social-button'><a href='#'>past events</a></div>
+        </div>
+        
+        <div class='events-white-container'>
+          <p>join the buzz</p>
+          <div class='social-button'><a href='#'>facebook</a></div>
+        </div>
 
       </div>  <!-- end of entry content-->
     </div>  <!-- end of container-->
