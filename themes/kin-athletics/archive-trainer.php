abd44fca8b4f31a4af9ca99547f1105ec0740adb
Kenneth Chow <kennethchow74@gmail.com>
2017-09-11 19:38:56 -0700

    added dynamic carousel functionality in archive-trainer.php

--- themes/kin-athletics/archive-trainer.php
+++ themes/kin-athletics/archive-trainer.php
@@ -52,37 +52,39 @@ get_header(); ?>
         </section>
 
         <div class="main-carousel">
-          <div class="carousel-cell trainers">
-            <?php
+            <?php 
                 $args = array( 'post_type' => 'trainer', 'order' => 'DESC', 'posts_per_page' => -1, 'orderby' => 'date' );
                 $trainer = get_posts( $args ); // returns an array of posts
                 $trainers_count = count($trainer);
                 $number_of_posts = 6;
                 $number_of_pages = $trainers_count / $number_of_posts;
-                
-                foreach ( $trainer as $post ) : setup_postdata( $post );
-            ?>
+                $trainer_total = $trainers_count;
 
-                <div class="trainer-item-container">
-                    <div class="trainer-thumbnail-wrapper">
-                        <img class="trainer-thumbnail" src="<?php echo CFS()->get( 'trainer_profile_picture' ); ?>">
-                    </div><!-- .trainer-thumbnail-wrapper -->
-                    <div class="next-trainer-wrapper">
-                        <a class="trainer-name" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a>
-                    </div><!-- .next-trainer-wrapper -->
+                for ($i = 0; $i < $number_of_pages; $i++) : ?>
+
+                <div class='carousel-cell trainers'> 
+                    <?php        
+                        $j = 0;
+                        while( $j < $number_of_posts && $trainer_total > 0 ) : 
+                    ?>
+            
+                    <div class='trainer-item-container'>
+                        <div class='trainer-thumbnail-wrapper'>
+                            <img class='trainer-thumbnail' src="<?php echo CFS()->get( 'trainer_profile_picture', $trainer[$j]->ID ); ?>"/>
+                        </div>
+                        <div class='next-trainer-wrapper'>
+                            <a class='trainer-name' href='<?php echo $trainer[$j]->guid ?>'><?php echo $trainer[$j]->post_title; ?></a>
+                        </div>
+                    </div> 
+
+                    <?php   
+                        $trainer_total--;
+                        $j++;
+                        endwhile;
+                        array_splice($trainer, 0, $number_of_posts);
+                    ?>
                 </div>
-            <?php endforeach; wp_reset_postdata(); ?>    
-          </div>  <!-- end of carousel cell personal training -->
-
-
-
-          <div class="carousel-cell group-training">
-          <?php 
-                for($i = 0 ; $i < $number_of_pages ; $i++) {
-                    echo $i.' ';
-                }
-            ?>
-          </div>  <!-- end of carousel cell group training -->
+            <?php endfor; ?>
         </div>  <!-- end of carousel -->
 
         <section class='our-community'>
