a80de6ec9893edd2647ddd96a5f03ce148aceeed
Kenneth Chow <kennethchow74@gmail.com>
2017-09-12 14:39:16 -0700

    added event post types.

--- plugins/kin-athletics-functionality/lib/functions/post-types.php
+++ plugins/kin-athletics-functionality/lib/functions/post-types.php
@@ -62,3 +62,60 @@ function kin_cpt_about() {
     
     }
     add_action( 'init', 'kin_cpt_about', 0 );
+
+// Register Custom Post Type
+function kin_cpt_events() {
+    
+        $labels = array(
+            'name'                  => 'Events',
+            'singular_name'         => 'Event',
+            'menu_name'             => 'Events',
+            'name_admin_bar'        => 'Events',
+            'archives'              => 'Event Archives',
+            'attributes'            => 'Event Attributes',
+            'parent_item_colon'     => 'Parent Event:',
+            'all_items'             => 'All Events',
+            'add_new_item'          => 'Add New Event',
+            'add_new'               => 'Add New',
+            'new_item'              => 'New Event',
+            'edit_item'             => 'Edit Event',
+            'update_item'           => 'Update Event',
+            'view_item'             => 'View Event',
+            'view_items'            => 'View Events',
+            'search_items'          => 'Search Event',
+            'not_found'             => 'Not found',
+            'not_found_in_trash'    => 'Not found in Trash',
+            'featured_image'        => 'Featured Image',
+            'set_featured_image'    => 'Set featured image',
+            'remove_featured_image' => 'Remove featured image',
+            'use_featured_image'    => 'Use as featured image',
+            'insert_into_item'      => 'Insert into item',
+            'uploaded_to_this_item' => 'Uploaded to this event',
+            'items_list'            => 'Events list',
+            'items_list_navigation' => 'Events list navigation',
+            'filter_items_list'     => 'Filter events list',
+        );
+        $args = array(
+            'label'                 => 'Event',
+            'description'           => 'Add an event page.',
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'revisions', ),
+            'hierarchical'          => true,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'menu_icon'             => 'dashicons-calendar-alt',
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => true,		
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'capability_type'       => 'post',
+            'show_in_rest'          => true,
+        );
+        register_post_type( 'event', $args );
+    
+    }
+    add_action( 'init', 'kin_cpt_events', 0 );
