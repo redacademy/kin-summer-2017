5b02bfe5f681381f3a9af33fc0604899b07deda3
Kenneth Chow <kennethchow74@gmail.com>
2017-09-13 14:21:35 -0700

    added sidebar to trainers.

--- themes/kin-athletics/template-parts/about-place.php
+++ themes/kin-athletics/template-parts/about-place.php
@@ -1,8 +1,18 @@
 <section class="page-section">
     <div class="header-container">
-        <h2 class="header-title">Our Origin</h2>
+        <h2 class="header-title">Our Place</h2>
     </div>
     <div class="description-container">
         <p class="item-description"><?php echo CFS()->get( 'about_our_place'); ?></p>
     </div>
+
+    <?php
+        if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+            return;
+        }
+    ?>
+
+    <div id="secondary" class="widget-area" role="complementary">
+        <?php dynamic_sidebar( 'sidebar-2' ); ?>
+    </div><!-- .secondary -->
 </section>
