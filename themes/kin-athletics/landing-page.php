e5db71ac1bc4c13453bfc3c06ad4f2f49b2227c6
Mark <marx.affiliate@gmail.com>
2017-09-13 10:19:47 -0700

    fixed landing page

--- themes/kin-athletics/landing-page.php
+++ themes/kin-athletics/landing-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Landing Page.
+ * Template Name: Landing Page
  *
  * @package Kin_Athletics_Theme
  */
@@ -8,3 +8,11 @@
 get_header(); ?>
 
 
+<div id='primary' class='content-area'>
+  <main id="main" class='site-main' role='main'>
+
+  
+  </main>
+</div>
+
+<?php get_footer(); ?>
