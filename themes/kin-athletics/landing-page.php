f0e77379cbc85012729c6082f230ca97e92c8b4c
Aleezay <aleezayh@hotmail.com>
2017-09-13 13:53:15 -0700

    repositioned Jump In section

--- themes/kin-athletics/landing-page.php
+++ themes/kin-athletics/landing-page.php
@@ -1,10 +1,15 @@
 <?php
 /**
- * Template Name: Landing Page.
+ * Template Name: Landing Page
  *
  * @package Kin_Athletics_Theme
  */
 
 get_header(); ?>
 
-
+<div class="landing-container">
+<h1 class="kin">kin</h1>
+<h1 class="ath">ath</h1>
+<h1 class="let">let</h1>
+<h1 class="ics">ics</h1>
+</div>
