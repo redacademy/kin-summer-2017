4cdd03c9c46ecb185d779ff70eff4f455a0de287
Matthew Cuthbert <matthew.cuthbert@live.com>
2017-09-16 16:16:53 -0700

    to get pull without conflict

--- themes/kin-athletics/functions.php
+++ themes/kin-athletics/functions.php
@@ -106,6 +106,8 @@ function kin_athletics_scripts() {
 
 	wp_enqueue_script( 'Flickity PACKAGED', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.js', array(), '2.0.9');
 
+	wP_enqueue_script( 'Flickity External', 'https://npmcdn.com/flickity@2/dist/flickity.pkgd.js');
+
 	wp_enqueue_script( 'Flickity', get_template_directory_uri() . '/build/js/flickity.min.js', array(), '20130115', true );	
 
 	wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/build/js/smooth-scroll.js', array(), '09/12/17', true );	
@@ -131,5 +133,7 @@ require get_template_directory() . '/inc/extras.php';
 remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
 
 include_once( get_stylesheet_directory() . '/inc/forms.php');
+
+include_once( get_stylesheet_directory() . '/inc/gforms-confirmation.php');
 
 add_filter('widget_text', 'do_shortcode');
