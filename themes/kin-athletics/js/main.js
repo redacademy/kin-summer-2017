492df833bbb26af8cbf4c7e1a0fc30c8ffc4b333
Mark <marx.affiliate@gmail.com>
2017-09-19 14:38:13 -0700

    fully styled and fully functional edit consult buttons

--- themes/kin-athletics/js/main.js
+++ themes/kin-athletics/js/main.js
@@ -5,42 +5,44 @@
     });
 
 
-
-
-
     $('#edit0').on('click',function(e){           // form 1
         e.preventDefault();
         $('#gform_page_1_6').toggle();
         $('#gform_page_1_1').toggle();
+        $('#gform_next_button_1_8').hide();
+        $('#edit-name').addClass('unhide-edit');
     });
 
     $('#edit1').on('click',function(e){           // form 2
         e.preventDefault();
         $('#gform_page_1_6').toggle();
         $('#gform_page_1_2').toggle();
+        $('#gform_next_button_1_10').hide();
+        $('#edit-email').addClass('unhide-edit');
     });
 
     $('#edit2').on('click',function(e){           // form 3
         e.preventDefault();
         $('#gform_page_1_6').toggle();
         $('#gform_page_1_3').toggle();
+        $('#gform_next_button_1_13').hide();
+        $('#edit-trainer').addClass('unhide-edit');
     });
 
     $('#edit3').on('click',function(e){           // form 4
         e.preventDefault();
         $('#gform_page_1_6').toggle();
         $('#gform_page_1_4').toggle();
+        $('#gform_next_button_1_12').hide();
+        $('#edit-action').addClass('unhide-edit');
     });
 
     $('#edit4').on('click',function(e){           // form 5
         e.preventDefault();
         $('#gform_page_1_6').toggle();
         $('#gform_page_1_5').toggle();
-    });
-
-    
-
-
-    
+        $('#gform_next_button_1_23').hide();
+        $('#edit-comment').addClass('unhide-edit');
+    });    
 
 })( jQuery );
