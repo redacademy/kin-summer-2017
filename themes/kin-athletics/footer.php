d709865a94cec765df79791f42f317b83a982791
Matthew Cuthbert <matthew.cuthbert@live.com>
2017-09-19 10:39:20 -0700

    finished desktop viw for find-us as well desktop view for footer

--- themes/kin-athletics/footer.php
+++ themes/kin-athletics/footer.php
@@ -11,9 +11,11 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class='footer-social-container'>
-					<a href='<?php echo esc_url( home_url( '/consultation/') )?>'>free consultation</a>
-					<a href='<?php echo esc_url( home_url( '/#/') )?>'>project td</a>
-					<a href='<?php echo esc_url( home_url( '/find-us/') )?>'>find us</a>
+					<div class='footer-text-container'>
+							<a href='<?php echo esc_url( home_url( '/consultation/') )?>'>free consultation</a>
+							<a href='<?php echo esc_url( home_url( '/#/') )?>'>project td</a>
+							<a href='<?php echo esc_url( home_url( '/find-us/') )?>'>find us</a>
+					</div><!-- .footer-text-container -->
 					<div class='footer-social-logo'>
 						<a href='https://www.facebook.com/kinathletics/' target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 						<a href='https://www.instagram.com/kinathletics/' target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
