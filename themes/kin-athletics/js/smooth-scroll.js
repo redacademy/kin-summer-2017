224b47ea249a486e6183adf4ab346f64e9e7925e
Kenneth Chow <kennethchow74@gmail.com>
2017-09-19 12:46:50 -0700

    added smooth scroll.

--- themes/kin-athletics/js/smooth-scroll.js
+++ themes/kin-athletics/js/smooth-scroll.js
@@ -1,40 +1,40 @@
-// (function($) {
-//     var target;
+(function($) {
+    var target;
 
-//     var jump=function(e)
-//     {
-//        if (e){
-//            e.preventDefault();
-//            target = $(this).attr("href");
-//        }else{
-//            target = location.hash;
-//        }
+    var jump=function(e)
+    {
+       if (e){
+           e.preventDefault();
+           target = $(this).attr("href");
+       }else{
+           target = location.hash;
+       }
     
-//        $('html,body').animate(
-//        {
-//            scrollTop: $(target).offset().top
-//        },2000,function()
-//        {
-//            location.hash = target;
-//        });
+       $('html,body').animate(
+       {
+           scrollTop: $(target).offset().top
+       },2000,function()
+       {
+           location.hash = target;
+       });
     
-//     }
+    }
     
-//     $('html, body').hide();
+    $('html, body').hide();
     
-//     $(document).ready(function()
-//     {
-//         $('a[href^=#]').bind("click", jump);
+    $(document).ready(function()
+    {
+        $('a[href^=#]').bind("click", jump);
     
-//         if (location.hash){
-//             setTimeout(function(){
-//                 $('html, body').scrollTop(0).show();
-//                 jump();
-//             }, 0);
-//         }else{
-//             $('html, body').show();
-//         }
-//     });
+        if (location.hash){
+            setTimeout(function(){
+                $('html, body').scrollTop(0).show();
+                jump();
+            }, 0);
+        }else{
+            $('html, body').show();
+        }
+    });
 
-// })( jQuery );
+})( jQuery );
     
