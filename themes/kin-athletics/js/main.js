5bb82ada5ae866c33ea3f9f48e37163dd8c12ac4
Mark <marx.affiliate@gmail.com>
2017-09-19 11:08:20 -0700

    nearly done consult logic and styled edit buttons

--- themes/kin-athletics/js/main.js
+++ themes/kin-athletics/js/main.js
@@ -5,11 +5,41 @@
     });
 
 
-    // Form consultation
-    $('#gf_step_1_5').on('click',function(e){
-        e.preventDefault(); 
-        alert($(this)); 
+
+
+
+    $('#edit0').on('click',function(e){           // form 1
+        e.preventDefault();
+        $('#gform_page_1_6').toggle();
+        $('#gform_page_1_1').toggle();
     });
+
+    $('#edit1').on('click',function(e){           // form 2
+        e.preventDefault();
+        $('#gform_page_1_6').toggle();
+        $('#gform_page_1_2').toggle();
+    });
+
+    $('#edit2').on('click',function(e){           // form 3
+        e.preventDefault();
+        $('#gform_page_1_6').toggle();
+        $('#gform_page_1_3').toggle();
+    });
+
+    $('#edit3').on('click',function(e){           // form 4
+        e.preventDefault();
+        $('#gform_page_1_6').toggle();
+        $('#gform_page_1_4').toggle();
+    });
+
+    $('#edit4').on('click',function(e){           // form 5
+        e.preventDefault();
+        $('#gform_page_1_6').toggle();
+        $('#gform_page_1_5').toggle();
+    });
+
+    
+
 
     
 
